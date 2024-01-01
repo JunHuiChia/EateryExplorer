27ebd19c757be5d6b43a1c8095cebf3d418c9b7c
Jun Hui Chia <jefferychia2014@gmail.com>
2024-01-01 18:20:13 +0000

    add basic button for retrieving data from server

--- web/src/routes/Index.jsx
+++ web/src/routes/Index.jsx
@@ -1,12 +1,19 @@
+function getEatery() {
+	fetch('http://localhost:3000/eatery')
+		.then((data) => data.json())
+		.then((data) => {
+			console.log(data);
+		});
+}
+
 function Index() {
-    return (
-        <div>
-        <h1>Home</h1>
-        <p>
-            Welcome to the home page!
-        </p>
-        </div>
-    )
+	return (
+		<div>
+			<h1>Home</h1>
+			<p>Welcome to the home page!</p>
+			<button onClick={getEatery}>Get Eatery</button>
+		</div>
+	);
 }
 
 export default Index;
