d4a660b15da11ab227e546d52629187ff65762ce
Jun Hui Chia <jefferychia2014@gmail.com>
2024-01-01 18:10:49 +0000

    add basic api route for retrieving mongoDB data

--- api/app.js
+++ api/app.js
@@ -3,14 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var eateryRouter = require('./routes/eatery');
 
 var app = express();
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.DB_URL;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,26 +27,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/eatery', eateryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(mongoDB);
+	await mongoose.connect(mongoDB);
 }
 
 module.exports = app;
