774d6bac9701abbfcfc10dc701c99f1e610b8ede
Jun Hui Chia <jefferychia2014@gmail.com>
2024-01-06 22:04:29 +0000

    update list schema

--- api/models/listModel.js
+++ api/models/listModel.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const listSchema = new Schema({
+    publicId: String,
     name: String,
+    owners: [String],
     category: [{type: Schema.Types.ObjectId, ref: 'Category'}],
 });
 
