1794f1f4383987c9040aa7ef7ce4823feb3304fe
Jun Hui Chia <jefferychia2014@gmail.com>
2024-01-13 20:56:47 +0000

    tidy up list page

--- web/src/routes/List.jsx
+++ web/src/routes/List.jsx
@@ -1,22 +1,26 @@
 import { useLoaderData } from 'react-router-dom';
 import ListApi from '../apis/ListApi';
+import CategoryBox from '../components/CategoryBox';
 
 async function loader({ params }) {
 	console.log(params);
-	return await ListApi.getOne(params.id);
+	var test = await ListApi.getOne(params.id);
+	console.log('test', test);
+	return test;
 }
 
 function List() {
 	const listData = useLoaderData();
+	console.log('List: ', listData);
 
 	return (
 		<div>
 			<h1>{listData.name}</h1>
-			<p>This is the list page!</p>
-			{listData.category.map((item) => (
-				<p key={item._id}>{item.name}</p>
-			))}
-			<p>{console.log(listData)}</p>
+			<h2>Categories:</h2>
+			{listData != null &&
+				listData.category.map((category) => (
+					<CategoryBox key={category._id} category={category} />
+				))}
 		</div>
 	);
 }
