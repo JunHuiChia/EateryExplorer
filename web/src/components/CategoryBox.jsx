90aadc7360360622c444afe51814eafaac9aba83
Jun Hui Chia <jefferychia2014@gmail.com>
2024-01-13 20:57:26 +0000

    add category box

---
+++ web/src/components/CategoryBox.jsx
@@ -0,0 +1,29 @@
+import '../css/list.css';
+import { useNavigate } from 'react-router-dom';
+
+function DisplayListInformation(data) {
+	console.log('CategoryBox:', data);
+	let eatery = data.category.eatery;
+	if (eatery.length === 0) eatery = 'No eateries';
+
+	return (
+		<>
+			<h2>{data.category.name}</h2>
+			{eatery !== 'No eateries' && <p>Eateries: {eatery.length}</p>}
+		</>
+	);
+}
+
+function CategoryBox(data) {
+	const navigate = useNavigate();
+	return (
+		<div
+			className='list'
+			onClick={() => navigate(`category/${data.category._id}`)}
+		>
+			{DisplayListInformation(data)}
+		</div>
+	);
+}
+
+export default CategoryBox;
