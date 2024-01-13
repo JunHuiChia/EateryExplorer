b63e9a6f2fdb81f9f7b4704fa7767a3cd3a774a1
Jun Hui Chia <jefferychia2014@gmail.com>
2024-01-13 21:56:31 +0000

    add logic in saveCategory API to update list with new category

--- api/services/category.js
+++ api/services/category.js
@@ -1,18 +1,25 @@
 const { categoryModel } = require('../models/categoryModel');
+const listModel = require('../models/listModel');
 
 async function getAllCategory() {
     return await categoryModel.find().populate('eatery').exec();
 }
 
 async function saveCategory(categoryData) {
-    console.log(categoryData.items);
+    console.log("saveCategory", categoryData);
+
     const newCategory = new categoryModel({
         name: categoryData.name,
         eatery: categoryData.eatery,
     });
 
     const category = await newCategory.save();
-    return category;
+
+    return await listModel.findByIdAndUpdate(
+        categoryData.listId,
+        { $push: { category: category._id } },
+        { new: true, useFindAndModify: false },
+    ).exec();
 }
 
 
