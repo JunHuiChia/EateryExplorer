94cec1e34dbe4d6dcbc690a00f1314e3ecf4fc1b
Jun Hui Chia <jefferychia2014@gmail.com>
2024-01-13 20:57:01 +0000

    add basic logging for main page

--- web/src/routes/Index.jsx
+++ web/src/routes/Index.jsx
@@ -20,6 +20,7 @@ async function loader() {
 function Index() {
 	const [newList, setNewList] = useState(false);
 	const testData = useLoaderData();
+	console.log('Index: ', testData);
 
 	useEffect(() => {
 		const userId = JSON.parse(localStorage.getItem('userId'));
