ed26f9cd00671dd946231a58672a089e032e7a4c
Jun Hui Chia <jefferychia2014@gmail.com>
2024-02-03 17:30:02 +0000

    add disclaimer to friends list of WIP

--- web/src/routes/FriendsList.jsx
+++ web/src/routes/FriendsList.jsx
@@ -1,9 +1,11 @@
 function FriendsList() {
-    return (
-        <div>
-            <h1>Friends List</h1>
-        </div>
-    )
+	return (
+		<div>
+			<h1>Friends List</h1>
+			<p>Welcome to the friends list page!</p>
+			<p>This is a work in progress. Come back in the future!</p>
+		</div>
+	);
 }
 
 export default FriendsList;
