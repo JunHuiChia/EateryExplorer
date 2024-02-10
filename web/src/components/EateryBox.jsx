9a43c0026b4532eae0c76bae85e89f25d09f0bd0
Jun Hui Chia <jefferychia2014@gmail.com>
2024-02-10 20:47:36 +0000

    add edit and delete icons to eatery box and adjust css for list boxes

--- web/src/components/EateryBox.jsx
+++ web/src/components/EateryBox.jsx
@@ -1,18 +1,35 @@
 import '../css/list.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
 
 function DisplayCategoryInformation(data) {
 	return (
 		<>
-			<h2>{data.eatery.name}</h2>
-			<p>{data.eatery.description}</p>
-			<p>Prices: {data.eatery.price}</p>
-			<p>Rating: {data.eatery.rating}</p>
+			<div className='box-data'>
+				<h2>{data.eatery.name}</h2>
+				<p>{data.eatery.description}</p>
+				<p>Prices: {data.eatery.price}</p>
+				<p>Rating: {data.eatery.rating}</p>
+			</div>
+			<div className='box-control'>
+				<FontAwesomeIcon
+					icon='fa-solid fa-pen-to-square'
+					className='fa-icons'
+				/>
+				<FontAwesomeIcon
+					icon='fa-solid fa-square-xmark'
+					className='fa-icons'
+				/>
+			</div>
 		</>
 	);
 }
 
-function CategoryBox(data) {
+function EateryBox(data) {
 	return <div className='list'>{DisplayCategoryInformation(data)}</div>;
 }
 
-export default CategoryBox;
+export default EateryBox;
