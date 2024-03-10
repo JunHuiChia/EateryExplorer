306a2919fb2f041d3e67ec021f837be2bcc0fbb0
Jun Hui Chia <jefferychia2014@gmail.com>
2024-03-10 17:37:17 +0000

    fix capitalisation of import

--- web/src/routes/App.jsx
+++ web/src/routes/App.jsx
@@ -1,5 +1,5 @@
 import '../css/App.css';
-import Navigation from '../components/navigation';
+import Navigation from '../components/Navigation';
 import { Outlet } from 'react-router-dom';
 
 function App() {
